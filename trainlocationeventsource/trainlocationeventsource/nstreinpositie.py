5915feb1a8e59f667a4ada1e442e97a33a0b7af9
Roel Adriaans <roel@adriaans.org>
2024-02-19 22:21:15 +0100

    Start working on the NStreinpositie message.
    Add the Dataclass for the NStreinpositie, add sample test data and the
    beginning of the testing framework for this message.

---
+++ trainlocationeventsource/trainlocationeventsource/nstreinpositie.py
@@ -0,0 +1,60 @@
+"""Convert messages from the NStreinpositiesInterface5 endpoint into a usable dict
+
+XML Spec defines thinks like TreinNummer, but this is not a pythonesque naming schema.
+XML entities are changes to be more Pythonic, eg lowercase and separated by underscores.
+TreinNummer will thus be renamed to trein_nummer.
+"""
+
+from __future__ import annotations
+
+from datetime import date, datetime
+
+import attrs
+
+
+@attrs.define(frozen=True)
+class NStreinpositie:
+    """Trein Positie"""
+
+    treinnummer: str
+    trein_materieel_delen: list[TreinMaterieelDeel]
+
+    @staticmethod
+    def from_xml(arrayoftreinlocation: str) -> list[NStreinpositie]:
+        """Create a list of NStreinposities from a raw XML stream."""
+        # @TODO Refactor this. Now we have business logic and XML parsing in the same
+        #    class..
+        ...
+
+        return []
+
+
+@attrs.define(frozen=True)
+class TreinMaterieelDeel:
+    """Materiaaldeel"""
+
+    materieel_deel_nummer: str
+    materieelvolgnummer: int
+    # Unixtimestamp. Always null?
+    generatie_tijd: int | None
+    gps_datumtijd: datetime
+    # This should be the travel oriantation. Spec says that the data is unreliable
+    # and not to use it.
+    orientatie: float | None
+    bronid: None
+    bron: str
+    # GPS Fix? 1 = Fix, 0 = no fix
+    fix: int
+    # Actuele Materieelpositie?
+    berichttype: None
+    # GPS Coordinates
+    longitude: float
+    latitude: float
+    elevation: float
+    snelheid: float
+    richting: float
+    # Currently not stable or provided
+    rijrichting: None
+    # Horizontale dilution of position.
+    hdop: float
+    aantal_satelieten: int
